72421d4714e34ca78de21d79ae5b9f4297eb459c
rasimrexhepi <rasim@myriadbymeta.com>
2023-12-14 16:48:49 +0100

    Reset height on delete text

--- src/form-elements/index.jsx
+++ src/form-elements/index.jsx
@@ -247,6 +247,9 @@ class TextArea extends React.Component {
 
   updateTextareaHeight = () => {
     const textarea = this.inputField.current;
+    if (textarea && textarea?.value?.length > 0) {
+      textarea.style.height = "";
+    }
     textarea.style.height = textarea.scrollHeight + "px"; // Set the height based on content
   };
 
