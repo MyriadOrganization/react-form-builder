ff201b697e116a72fe8d2a68aae76a30886c816e
Enis Tahiraj <enistahiraj59@gmail.com>
2024-01-16 09:16:46 -0500

    custom form on change

--- src/form-elements/custom-element.jsx
+++ src/form-elements/custom-element.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
-import ComponentHeader from './component-header';
-import ComponentLabel from './component-label';
+import React, { Component } from "react";
+import ComponentHeader from "./component-header";
+import ComponentLabel from "./component-label";
 
 class CustomElement extends Component {
   constructor(props) {
     super(props);
     this.inputField = React.createRef();
   }
+
+  handleInternalChange = (newValue) => {
+    const event = {
+      target: {
+        name: this.props.data.field_name,
+        value: newValue,
+      },
+    };
+
+    if (this.props.handleChange) {
+      this.props.handleChange(event);
+    }
+  };
 
   render() {
     const { bare } = this.props.data;
@@ -19,30 +32,39 @@ class CustomElement extends Component {
     }
 
     if (this.props.read_only) {
-      props.disabled = 'disabled';
-    }
-
-    if (this.props.mutable) {
-      props.onChange = this.props.handleChange;
+      props.disabled = "disabled";
     }
 
     // Return if component is invalid.
     if (!this.props.data.component) return null;
     const Element = this.props.data.component;
 
-    let baseClasses = 'SortableItem rfb-item';
-    if (this.props.data.pageBreakBefore) { baseClasses += ' alwaysbreak'; }
+    let baseClasses = "SortableItem rfb-item";
+    if (this.props.data.pageBreakBefore) {
+      baseClasses += " alwaysbreak";
+    }
 
     return (
       <div className={baseClasses}>
         <ComponentHeader {...this.props} />
-        { bare ?
-          <Element data={this.props.data} {...this.props.data.props} {...props} /> :
+        {bare ? (
+          <Element
+            data={this.props.data}
+            onChange={this.handleInternalChange}
+            {...this.props.data.props}
+            {...props}
+          />
+        ) : (
           <div className="form-group">
             <ComponentLabel className="form-label" {...this.props} />
-            <Element data={this.props.data} {...this.props.data.props} {...props} />
+            <Element
+              data={this.props.data}
+              onChange={this.handleInternalChange}
+              {...this.props.data.props}
+              {...props}
+            />
           </div>
-        }
+        )}
       </div>
     );
   }
