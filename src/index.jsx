8700b440e54ff029f8ccd436dff4bc663292683a
Rasim Rexhepi <rasim@myriadbymeta.com>
2024-07-16 15:38:32 +0200

    add font in index

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,7 @@ import store from "./stores/store";
 import Registry from "./stores/registry";
 import AppLocale from "./language-provider";
 import "./bootstrap.scss";
+import "./font-awesome.scss";
 
 class ReactFormBuilder extends React.Component {
   constructor(props) {
