ae33a22cc88408372c877eddf9266055d824693f
Rasim Rexhepi <rasim@myriadbymeta.com>
2024-07-16 15:12:42 +0200

    fix bug style prop null

--- src/form-elements/index.jsx
+++ src/form-elements/index.jsx
@@ -248,12 +248,10 @@ class TextArea extends React.Component {
 
   updateTextareaHeight = () => {
     const textarea = this.inputField.current;
-    if (textarea.style) {
-      if (textarea && textarea?.value?.length > 0) {
-        textarea.style.height = "";
-      }
-      textarea.style.height = textarea.scrollHeight + "px"; // Set the height based on content
+    if (textarea && textarea?.value?.length > 0) {
+      textarea.style.height = "";
     }
+    textarea.style.height = textarea.scrollHeight + "px"; // Set the height based on content
   };
 
   handleChange = (e) => {
@@ -275,7 +273,7 @@ class TextArea extends React.Component {
     if (this.props.mutable) {
       props.defaultValue = this.props.defaultValue;
       props.ref = this.inputField;
-      props.onChange = this.props.handleChange;
+      props.onChange = this.handleChange;
     }
 
     let baseClasses = "SortableItem rfb-item";
