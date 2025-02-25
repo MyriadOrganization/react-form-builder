9b280ce30d2ae4b91854bd955c136b79c60db00a
Elidon Demolli <edemolli@myriadbymeta.com>
2025-02-25 15:44:48 +0100

    all data onChange

--- src/form.jsx
+++ src/form.jsx
@@ -2,14 +2,14 @@
  * <Form />
  */
 
+import { EventEmitter } from "fbemitter";
 import React from "react";
 import ReactDOM from "react-dom";
-import { EventEmitter } from "fbemitter";
 import { injectIntl } from "react-intl";
-import FormValidator from "./form-validator";
 import FormElements from "./form-elements";
-import { TwoColumnRow, ThreeColumnRow, FourColumnRow } from "./multi-column";
 import CustomElement from "./form-elements/custom-element";
+import FormValidator from "./form-validator";
+import { FourColumnRow, ThreeColumnRow, TwoColumnRow } from "./multi-column";
 import Registry from "./stores/registry";
 
 const { Image, Checkboxes, Signature, Download, Camera, FileUpload } =
@@ -63,11 +63,12 @@ class ReactForm extends React.Component {
     return defaultChecked;
   }
 
-  _getItemValue(item, ref) {
+  _getItemValue(item, ref, shouldTrim = true) {
     let $item = {
       element: item.element,
       value: "",
     };
+    
     if (item.element === "Rating") {
       $item.value = ref.inputField.current.state.rating;
     } else if (item.element === "Tags") {
@@ -83,7 +84,13 @@ class ReactForm extends React.Component {
     } else if (ref && ref.inputField && ref.inputField.current) {
       $item = ReactDOM.findDOMNode(ref.inputField.current);
       if ($item && typeof $item.value === "string") {
-        $item.value = $item.value.trim();
+        if(shouldTrim) {
+          console.log('trim')
+          $item.value = $item.value.trim();
+        } else {
+          console.log('no trim')
+          $item.value = $item.value;
+        }
       }
     }
     return $item;
@@ -153,7 +160,7 @@ class ReactForm extends React.Component {
     return invalid;
   }
 
-  _collect(item) {
+  _collect(item, shouldTrim = true) {
     const itemData = {
       name: item.field_name,
       custom_name: item.custom_name || item.field_name,
@@ -173,15 +180,15 @@ class ReactForm extends React.Component {
       itemData.value = checked_options;
     } else {
       if (!ref) return null;
-      itemData.value = this._getItemValue(item, ref).value;
+      itemData.value = this._getItemValue(item, ref, shouldTrim).value;
     }
     return itemData;
   }
 
-  _collectFormData(data) {
+  _collectFormData(data, shouldTrim = true) {
     const formData = [];
     data?.forEach((item) => {
-      const item_data = this._collect(item);
+      const item_data = this._collect(item, shouldTrim);
       if (item_data) {
         formData.push(item_data);
       }
@@ -229,7 +236,9 @@ class ReactForm extends React.Component {
   }
 
   handleChange = (evt) => {
-    this.props.onChange && this.props.onChange(evt);
+    const whole_response = this._collectFormData(this.props.data, false);
+    console.log({ whole_response })
+    this.props.onChange && this.props.onChange(evt, whole_response);
   };
 
   validateForm() {
